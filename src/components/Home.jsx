f99e2d4695d0f4d2a57138afe9adca402ce37d3c
sylvain joly <sylvain.joly00@orange.fr>
2019-10-25 13:43:21 +0200

    WIP

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,21 +1,51 @@
 import React from 'react';
-import {Container} from "react-bootstrap";
-
+import {Col, Container, Form, Row} from "react-bootstrap";
 
 
 export default class Home extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-        };
+        this.state = {};
 
 
     }
+
     render() {
         return (
             <div>
                 <Container>
-                    <p>HOME</p>
+                    <Row>
+                        <Col>
+                            <Form.Group controlId="formSearchAnnounce">
+                                <Form.Label>Localiastion</Form.Label>
+                                <Form.Control type="text"/>
+                            </Form.Group>
+                        </Col>
+                        <Col>
+                            <Form.Group controlId="formSelectAnimalType">
+                                <Form.Label>Type d'animaux</Form.Label>
+                                <Form.Control as="select">
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                    <option>5</option>
+                                </Form.Control>
+                            </Form.Group>
+
+                            <Form.Group controlId="formSelectService">
+                                <Form.Label>Service</Form.Label>
+                                <Form.Control as="select">
+                                    <option>1</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                    <option>5</option>
+                                </Form.Control>
+                            </Form.Group>
+
+                        </Col>
+                    </Row>
                 </Container>
             </div>
         )
